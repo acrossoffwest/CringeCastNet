1bf8214c9b85ef0eb7658ec7219c8a4fba16a9e3
Yurij Karpov <acrossoffwest@gmail.com>
2023-03-25 22:06:16 +0100

    fix

--- utils/remove-service.sh
+++ utils/remove-service.sh
@@ -2,10 +2,5 @@
 
 if [ -f /etc/systemd/system/cringecast.service ]
 then
-    if (systemctl -q is-active cringecast.service)
-    then
-      systemctl stop cringecast.service
-    fi
     systemctl disable cringecast.service
-    rm /etc/systemd/system/cringecast.service
 fi
