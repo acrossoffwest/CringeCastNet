ad2863af660c6bb425ba0a451f15aec5ccd704a1
Yurij Karpov <acrossoffwest@gmail.com>
2023-03-25 08:10:42 +0100

    add amd64 builda for windows

--- client/build.sh
+++ client/build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 GOOS=darwin GOARCH=arm64 go build -o ../bin/cringecast-client main.go
+GOOS=windows GOARCH=amd64 go build -o ../bin/cringecast-client-win.exe main.go
--- server/build.sh
+++ server/build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 GOOS=darwin GOARCH=arm64 go build -o ../bin/cringecast-server main.go
+GOOS=windows GOARCH=amd64 go build -o ../bin/cringecast-server-win.exe main.go
