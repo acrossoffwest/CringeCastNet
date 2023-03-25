fa17a6e64a83cb459a203ceaae6a6622866699cc
Yurij Karpov <acrossoffwest@gmail.com>
2023-03-25 11:23:40 +0100

    add arm v7 client test for pi

--- client/build-linux.sh
+++ client/build-linux.sh
@@ -5,3 +5,6 @@ docker run --rm -it --platform=linux/amd64 -v $(pwd)/../bin:/output cringecast-c
 
 docker build --platform=linux/arm64 -t cringecast-client-linux-build-arm .
 docker run --rm -it --platform=linux/arm64 -v $(pwd)/../bin:/output cringecast-client-linux-build-arm /bin/sh -c "cp /app/cringecast-client-linux /output/cringecast-client-linux-arm"
+
+docker build --platform=linux/arm/v7 -t cringecast-client-linux-build-arm-v7 .
+docker run --rm -it --platform=linux/arm/v7 -v $(pwd)/../bin:/output cringecast-client-linux-build-arm-v7 /bin/sh -c "cp /app/cringecast-client-linux /output/cringecast-client-linux-arm-v7"
